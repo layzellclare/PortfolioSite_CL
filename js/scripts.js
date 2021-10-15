681eee1cdae88415a1d9edb9c4180f49c6a31c5e
layzellclare <92582482+layzellclare@users.noreply.github.com>
2021-10-15 13:28:14 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+console.log("Hi - Welcome to my UX Design site")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
